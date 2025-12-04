82b1cc113941d1f9baedd09f14ff3f7eafe90f08
flodlb <98769183+flodlb@users.noreply.github.com>
2025-12-04 11:51:51 +0100

    Remove commented-out config import and related code

--- staticFiles.js
+++ staticFiles.js
@@ -1,11 +1,5 @@
-/*import config from "./config.json" assert { type: "json" };
+import config from './config.json' with { type: 'json' }
 
 export {
   config,
-}*/
-
-import { readFileSync } from "fs";
-
-export const config = JSON.parse(
-  readFileSync(new URL("./config.json", import.meta.url), "utf8")
-);
+}
