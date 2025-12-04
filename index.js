e5e097d2d325aac6c9cbbb64a2d2f9ffa320eca5
flodlb <98769183+flodlb@users.noreply.github.com>
2025-12-04 16:40:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function getAntiBotToken(page, timeout = 20000) {
       await page.mouse.move(Math.random() * 300, Math.random() * 100)
       await page.mouse.move(Math.random() * 300, 500 + Math.random() * 100)
       await page.evaluate(() => window.scrollBy(0, 100 + Math.random() * 80))
-    } catch (err) {
+    } catch {
       console.log('nul')
     }
     await page.waitForTimeout(200)
