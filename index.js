c7c792752a25993638384721bd546c1a6d5d62eb
flodlb <98769183+flodlb@users.noreply.github.com>
2025-12-04 14:12:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ async function getAntiBotToken(page, timeout = 2000) {
       await page.waitForTimeout(40)
       await page.mouse.up()
       await page.evaluate(() => window.scrollBy(0, 120))
-    } catch {}
+    } catch {
+      console.log('texte')
+    }
   }
 
   throw new Error('❌ Impossible de récupérer le token anti-bot.')
