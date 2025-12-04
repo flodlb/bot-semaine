7d991894259d76dfb36fcf90c8b6f55ac0c27fde
flodlb <98769183+flodlb@users.noreply.github.com>
2025-12-04 13:46:33 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin-js'
 
-export default [
-  {
-    languageOptions: { globals: globals.node },
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.browser
+    }
   },
   {
     plugins: {
